d12718103dc58d0f5dee8b3c925b0b4724afa781
pjpjpj <877302410@qq.com>
2018-01-11 00:04:35 +0800

    完成 新闻样式匹配tab切换新闻数据

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,30 +1,64 @@
 
 Page({
   data: {
-    resData: []
+    resData: [],
+    titleData_en: ['zhxw', 'tpxw', 'rcpy', 'jxky', 'whhd', 'xyrw', 'jlhz', 'shfw', 'mtgz'],
+    titleData_cn: ['综合新闻', '图片新闻', '人才培养', '教学科研', '文化活动', '校园人物', '交流合作', '社会服务', '媒体关注'],
+    titleIsHiddens: [false, true, true, true, true, true, true, true, true, true, true],
+    titleIndex: 0
   },
+
   onLoad: function () {
     var that = this
     wx.request({
       url: 'https://api.iflab.org/api/v2/newsapi/newslist',
       method: 'GET',
-      data: { 
+      data: {
         category: 'zhxw',
         page: 0,
         api_key: '3528bd808dde403b83b456e986ce1632d513f7a06c19f5a582058be87be0d8c2',
-        session_token: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIzYTE4OWM0NDZhOWNlMzQ0M2NjMDQ1YmQyZTM4ZDA4YyIsImlzcyI6Imh0dHBzOi8vYXBpLmlmbGFiLm9yZy9hcGkvdjIvdXNlci9zZXNzaW9uIiwiaWF0IjoxNTE1NTAzNzg1LCJleHAiOjE1MTU1OTAxODUsIm5iZiI6MTUxNTUwMzc4NSwianRpIjoiS1A1d3EzbXdtSVVMaGdxUCIsInVzZXJfaWQiOjQyLCJmb3JldmVyIjpmYWxzZX0.j9uU1czLTx7aWDl7-b1Nf5vVbMtcWWsJgadf7v251DA'
+        session_token: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIzYTE4OWM0NDZhOWNlMzQ0M2NjMDQ1YmQyZTM4ZDA4YyIsImlzcyI6Imh0dHBzOi8vYXBpLmlmbGFiLm9yZy9hcGkvdjIvdXNlci9zZXNzaW9uIiwiaWF0IjoxNTE1NTQzMjc0LCJleHAiOjE1MTU2Mjk2NzQsIm5iZiI6MTUxNTU0MzI3NCwianRpIjoiekpvY04xT211MEwwT3RSUCIsInVzZXJfaWQiOjQyLCJmb3JldmVyIjpmYWxzZX0.GMnfmQPEr2FnUPrK9oBocV63CS8kSwKC1HhYuuKqz_4'
       },
       success: function (res) {
-        // console.log(res)
         that.setData({
-          resData : res.data
+          resData: res.data
         })
+
         console.log(res.data)
       }, fail: function (res) {
 
       }, complete: function () {
 
       }
-    }) 
+    })
+  },
+
+  titleHeadViewTapClick: function (event) {
+    var that = this
+    that.data.titleIndex = event.currentTarget.id
+    this.setData({
+      titleIndex: this.data.titleIndex
+    })
+    wx.request({
+      url: 'https://api.iflab.org/api/v2/newsapi/newslist',
+      method: 'GET',
+      data: {
+        category: this.data.titleData_en[event.currentTarget.id],
+        page: 0,
+        api_key: '3528bd808dde403b83b456e986ce1632d513f7a06c19f5a582058be87be0d8c2',
+        session_token: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIzYTE4OWM0NDZhOWNlMzQ0M2NjMDQ1YmQyZTM4ZDA4YyIsImlzcyI6Imh0dHBzOi8vYXBpLmlmbGFiLm9yZy9hcGkvdjIvdXNlci9zZXNzaW9uIiwiaWF0IjoxNTE1NTQzMjc0LCJleHAiOjE1MTU2Mjk2NzQsIm5iZiI6MTUxNTU0MzI3NCwianRpIjoiekpvY04xT211MEwwT3RSUCIsInVzZXJfaWQiOjQyLCJmb3JldmVyIjpmYWxzZX0.GMnfmQPEr2FnUPrK9oBocV63CS8kSwKC1HhYuuKqz_4'
+      },
+      success: function (res) {
+        that.setData({
+          resData: res.data
+        })
+        console.log(res)
+      }, fail: function (res) {
+
+      }, complete: function () {
+
+      }
+    })
   }
+
 })
